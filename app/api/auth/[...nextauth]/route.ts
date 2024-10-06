c3d45699ccaf16fd9f7f5210626770bb3aaf0840
Devastati0n <havokcs1@yahoo.com>
2024-10-06 16:55:54 -0700

    first commit

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,17 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import GithubProvider from "next-auth/providers/github"
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
     // ...add more providers here
   ],
 }
 export default NextAuth(authOptions)
+
+//https://github.com/Devastati0n/superbowl2.git
+
